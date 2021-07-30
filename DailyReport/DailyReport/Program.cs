b92b686eef56aa38ba13990a4609226a9e445997
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-07-30 14:04:43 -0700

    DailyReport resubmit

--- DailyReport/DailyReport/Program.cs
+++ DailyReport/DailyReport/Program.cs
@@ -16,15 +16,14 @@ namespace DailyReport
 
             Console.WriteLine("\nWhat page number?");
             string pageNum = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(pageNum);
 
             // if the student answers yes, then the needHelp variable will be 
             // set to true and can be used later to generate different responses
-            Console.WriteLine("\nDo you need help with anything? Please answer “yes” or “no.”");
+            Console.WriteLine("\nDo you need help with anything? Please answer “true” or “false.”");
             string helpMe = Console.ReadLine();
-            bool needHelp = false;
-            if (helpMe == "yes") {
-                needHelp = true;
-            }
+            bool helpNeeded = Convert.ToBoolean(helpMe);
+            Console.WriteLine(helpNeeded);
 
             Console.WriteLine("\nWere there any positive experiences you’d like to share? Please give specifics.");
             string experiences = Console.ReadLine();
@@ -34,6 +33,7 @@ namespace DailyReport
 
             Console.WriteLine("\nHow many hours did you study today?");
             string studyHours = Console.ReadLine();
+            int hours = Convert.ToInt32(studyHours);
 
             // Output a message with user input included
             Console.WriteLine("\nWe are glad to see you put in " + studyHours + " hours of work on " + course + " today, and that you made it all the way to page " + pageNum + "!");
