89d0ec44a072dffa6b15739052f9a57eeb2d05e2
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-08-02 15:13:40 -0700

    DoWhile

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            int ii = 1;
+            // Output value of ii and increment until 10
+            while (ii <= 10)
+            {
+                Console.WriteLine(ii);
+                ii++;
+            }
+
+            // Decrement value of ii and output value until zero
+            do
+            {
+                ii--;
+                Console.WriteLine(ii);
+            }
+            while (ii >= 1);
+
+
+            
+        }
+    }
+}
