79d2a3dbcde84c5b2b440dee699cbf8e1a0b96bc
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-08-04 13:05:36 -0700

    TryCatch

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>() { 1, 2, 3, 4, 5, 10, 20, 100 };
+
+            try
+            {
+                Console.WriteLine("Please input an integer to divide by:");
+                int divideBy = Convert.ToInt32(Console.ReadLine());
+                foreach (int num in myList)
+                {
+                    Console.WriteLine(num / divideBy);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("You did not provide an integer.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+
+        }
+    }
+}
