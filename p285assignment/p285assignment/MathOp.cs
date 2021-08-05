9fbdb2db6fd02736fe106268c23cf6cdea1479d9
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-08-05 14:20:27 -0700

    p285 assignment

--- p285assignment/p285assignment/MathOp.cs
+++ p285assignment/p285assignment/MathOp.cs
@@ -6,9 +6,37 @@ namespace p285assignment
 {
     class MathOp
     {
+        public static string thankYouMsg;
+        public static string halved;
+
+        static MathOp()
+        {
+            thankYouMsg = "Thank you for using my program.";
+            halved = "You successfully halved your integer.";
+        }
+
+        // Method takes integer parameter
         public void DivideByTwo(int value)
         {
+            // Divide input value by 2
             Console.WriteLine("{0} divided by 2 is {1}", value, value / 2);
+            // Call static class
+            Console.WriteLine(MathOp.halved);
+        }
+
+        // Method takes string parameter
+        public void DivideByTwo(string value)
+        {
+            Console.WriteLine("{0} divided by 2 is {1}", value, (Convert.ToInt32(value) / 2));
+            Console.WriteLine(MathOp.halved);
+        }
+
+        public void Operations(int value, out int xAdd, out int xSub, out int xMult, out int xDiv)
+        {
+            xAdd = value + 2;
+            xSub = value - 2;
+            xMult = value * 2;
+            xDiv = value / 2;
         }
     }
 }
--- p285assignment/p285assignment/Program.cs
+++ p285assignment/p285assignment/Program.cs
@@ -10,9 +10,26 @@ namespace p285assignment
 
             Console.WriteLine("Please enter an integer:");
             int value = Convert.ToInt32(Console.ReadLine());
-
             mathOp.DivideByTwo(value);
 
+            Console.WriteLine("Please enter another integer:");
+            string valueStr = Console.ReadLine();
+            mathOp.DivideByTwo(valueStr);
+
+            Console.WriteLine("\nPlease enter an integer to perform various operations to:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            // Declare variables
+            int xAdd = 0, xSub = 0, xMult = 0, xDiv = 0;
+            // Call Operations method and assign new values to above variables using the out paramaters
+            mathOp.Operations(x, out xAdd, out xSub, out xMult, out xDiv);
+            Console.WriteLine("{0} + 2 = {1}", x, xAdd);
+            Console.WriteLine("{0} - 2 = {1}", x, xSub);
+            Console.WriteLine("{0} * 2 = {1}", x, xMult);
+            Console.WriteLine("{0} / 2 = {1}", x, xDiv);
+
+
+            // Calling the static class again just to show it can be called here as well
+            Console.WriteLine(MathOp.thankYouMsg);
 
 
 
