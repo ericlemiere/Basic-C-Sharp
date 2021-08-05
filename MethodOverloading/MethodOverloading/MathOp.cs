75e1fe9fa80d061b3319175c0c95cef237dc3703
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-08-05 10:50:10 -0700

    Added to MethodOverloading
    Added class with method that has optional parameters

---
+++ MethodOverloading/MethodOverloading/MathOp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOverloading
+{
+    class MathOp
+    {
+        public int Multiply(int value1, int value2 = 1)
+        {
+            if (value2 == 1) {
+                value2 = value1;
+            }
+            return value1 * value2;
+        } 
+    }
+}
--- MethodOverloading/MethodOverloading/Program.cs
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -6,19 +6,46 @@ namespace MethodOverloading
     {
         static void Main(string[] args)
         {
-            SquareClass square = new SquareClass();
+            //SquareClass square = new SquareClass();
 
-            Console.WriteLine("\nEnter an integer you would like squared: ");
-            int squareInt = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n{0}^2 = {1}", squareInt, square.Square(squareInt));
+            //Console.WriteLine("\nEnter an integer you would like squared: ");
+            //int squareInt = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("\n{0}^2 = {1}", squareInt, square.Square(squareInt));
 
-            Console.WriteLine("\nEnter a decimal you would like squared: ");
-            decimal squareDec = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("\n{0}^2 = {1}", squareDec, square.Square(squareDec));
+            //Console.WriteLine("\nEnter a decimal you would like squared: ");
+            //decimal squareDec = Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine("\n{0}^2 = {1}", squareDec, square.Square(squareDec));
 
-            Console.WriteLine("\nEnter a number you would like squared: ");
-            string squareStr = Console.ReadLine();
-            Console.WriteLine("\n{0}^2 = {1}", squareStr, square.Square(squareStr));
+            //Console.WriteLine("\nEnter a number you would like squared: ");
+            //string squareStr = Console.ReadLine();
+            //Console.WriteLine("\n{0}^2 = {1}", squareStr, square.Square(squareStr));
+
+            MathOp multiply = new MathOp();
+            Console.WriteLine("\nNow you will enter two new values to multiply together:");
+            Console.WriteLine("Value 1:");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Value 2: (Press enter to skip. Output will be the first value squared.)");
+
+            try
+            {
+                string value2Str = Console.ReadLine();
+                if (value2Str != "")
+                {
+                    int value2 = Convert.ToInt32(value2Str);
+                    Console.WriteLine("{0} * {1} = {2}", value1, value2, multiply.Multiply(value1, value2));
+                }
+                else
+                {
+                    Console.WriteLine("{0}^2 = {1}", value1, multiply.Multiply(value1));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Input Error");
+            }
+
+            
+
 
         }
     }
