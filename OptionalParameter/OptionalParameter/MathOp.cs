66c1d2c25b265b2d561fd8ee60c1e43a06b478a0
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-08-05 13:39:32 -0700

    OptionalParameter assignment

---
+++ OptionalParameter/OptionalParameter/MathOp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionalParameter
+{
+    class MathOp
+    {
+        public int Multiply(int value1, int value2 = 1)
+        {
+            if (value2 == 1)
+            {
+                value2 = value1;
+            }
+            return value1 * value2;
+        }
+    }
+}
---
+++ OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOp multiply = new MathOp();
+            Console.WriteLine("\nNow you will enter two new values to multiply together:");
+            Console.WriteLine("Value 1:");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Value 2: (Press enter to skip. Output will be the first value squared.)");
+
+            try
+            {
+                string value2Str = Console.ReadLine();
+                if (value2Str != "")
+                {
+                    int value2 = Convert.ToInt32(value2Str);
+                    Console.WriteLine("{0} * {1} = {2}", value1, value2, multiply.Multiply(value1, value2));
+                }
+                else
+                {
+                    Console.WriteLine("{0}^2 = {1}", value1, multiply.Multiply(value1));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Input Error");
+            }
+        }
+    }
+}
