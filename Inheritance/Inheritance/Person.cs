072b0fabf11a836970a9a5526940fd17669a6ed0
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-08-06 08:44:05 -0700

    Inheritance assignment

---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        // Declare public variables
+        public string FirstName;
+        public string LastName;
+        // Method to print name to console
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize first employee with id and name
+            Employee employee1 = new Employee() { id = 1, FirstName = "Sample", LastName = "Student" };
+            // Print name to console
+            employee1.SayName();
+        }
+    }
+}
