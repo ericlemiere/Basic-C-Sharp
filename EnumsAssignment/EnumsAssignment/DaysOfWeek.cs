3a21612960d011cd3321b2bb069666d95fb2c825
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-08-09 16:23:31 -0700

    EnumsAssignment

---
+++ EnumsAssignment/EnumsAssignment/DaysOfWeek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumsAssignment
+{
+    public class DaysOfWeek
+    {
+        //public DayOfWeek Day { get; set; }
+        public enum DayOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
---
+++ EnumsAssignment/EnumsAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            try
+            {            
+                Console.WriteLine("Please enter the current day of the week: ");
+                string dayEntered = Console.ReadLine();
+                // Instantiate DayOfWeek object
+                // Check user input to see if it is in the enum
+                // True ignores case of string
+                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), dayEntered, true);
+                Console.WriteLine("\nToday is {0}.", day);
+            }
+            catch 
+            {
+                Console.WriteLine("\nPlease enter an actual day of the week.");
+            }
+
+        }
+    }
+}
