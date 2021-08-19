4bc7dd2a183bc6e1efc8cff716669ecb1bdc9d03
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-08-18 19:39:13 -0700

    Final Challenge Updated and Working

--- FinalChallenge2/FinalChallenge2/Program.cs
+++ FinalChallenge2/FinalChallenge2/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
 
 namespace FinalChallenge2
 {
@@ -6,14 +6,18 @@ namespace FinalChallenge2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter New Student's Name:");
+            string name = Console.ReadLine();
+
             using (var db = new SchoolContext())
             {
                 var student = new Student();
-                student.StudentName = "Bill";
+                student.StudentName = name;
 
                 db.Students.Add(student);
                 db.SaveChanges();
             }
+            Console.WriteLine("{0} added", name);
         }
     }
 
--- FinalChallenge2/FinalChallenge2/SchoolContext.cs
+++ FinalChallenge2/FinalChallenge2/SchoolContext.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Text;
 
 namespace FinalChallenge2
